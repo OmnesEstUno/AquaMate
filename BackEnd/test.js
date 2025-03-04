231e9c930d6e578160097e4f2c8cff83221bc907
Elliot Warren <elliotjwarren@gmail.com>
2025-03-03 20:29:51 -0800

    update react build scripts, clean up gitignore

--- BackEnd/test.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json({ message: "Test passed" }); // Send the message in an object
-});
-
-module.exports = router;
